6e19ba1fb602b6e1136f018b12d8440443118e71
John73x <46722057+John73x@users.noreply.github.com>
2020-02-14 17:43:43 +0100

    zweite uebung

---
+++ uebung2.py
@@ -0,0 +1,16 @@
+print("gebe dein Alter ein")
+Alter=int(input())
+if Alter <=17:
+    print("du bist minderjährig")
+
+if Alter >17:
+    print("Du bist volljährig. Glueckwunsch:)")
+
+if Alter >100:
+    print("Boah, bist du alt!!!")
+
+if Alter >1000:
+    print ("Warte! So alt? Mooooooment! DAS glaube ich dir aber nicht")
+
+
+
