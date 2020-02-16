04fa40fc57b4321cc9c664df7ef9289fd8ec2a1d
John73x <46722057+John73x@users.noreply.github.com>
2020-02-16 11:51:50 +0100

    uebung2b

--- uebung2c.py
+++ uebung2c.py
@@ -1,6 +1,6 @@
 print("Gebe eine Zahl ein")
 Erste=int(input())
-print ("Gebe noch eine Zahl ein")
+print("Gebe noch eine Zahl ein")
 Zweite=int(input())
 res=Erste+Zweite
 print("Ergebnis =", res);
