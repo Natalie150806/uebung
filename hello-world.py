463f7b527ea6a6caacca743944e5c51a4e253d8d
John73x <46722057+John73x@users.noreply.github.com>
2020-02-16 11:00:35 +0100

    hello_world

--- hello-world.py
+++ hello-world.py
@@ -1,7 +1,7 @@
 def hello():
     print("gebe deinen namen ein: ")
     name=input()
-    print("hallo " + name + "wie geht´s dir so?")
+    print("hallo " + name + ", wie geht´s dir so?")
 
 
 if __name__ == '__main__':
